ad7bed34c87fc9afaf85dc5f33788a073250c492
logossoudarryl20 <logossoudarryl20@gmail.com>
2025-03-12 11:05:14 +0000

    update wather screen design--add linear gradient background color

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,14 +5,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(),
-      body: const Center(
-        child: Text(
-          "Welcome",
-          style: TextStyle(
-            color: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+        // extendBodyBehindAppBar: true,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF0F1320), // Bleu très foncé presque noir en haut
+                Color(0xFF161B2F), // Bleu foncé légèrement plus clair en bas
+              ],
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: const AlignmentDirectional(3, 6),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        // color: Colors.deepPurple,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
