3c31b2505b7644cb324a6bfde9548a33375bdc7b
Darrylwin <logossoudarryl20@gmail.com>
2025-03-14 13:22:52 +0000

    feat: enhance Forecast component with dynamic temperature and weather display

--- lib/components/forecast.dart
+++ lib/components/forecast.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Forecast extends StatelessWidget {
-  const Forecast({super.key});
+  const Forecast({
+    super.key,
+    required this.temperature,
+    required this.weatherMain,
+    required this.day,
+  });
+
+  final int temperature;
+  final String weatherMain;
+  final String day;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(6),
-      width: 60,
+      padding: const EdgeInsets.all(9),
+      width: 73,
       decoration: const BoxDecoration(
         color: Color(0xff1E1D2B),
         borderRadius: BorderRadius.all(
@@ -20,36 +30,33 @@ class Forecast extends StatelessWidget {
             "assets/icons/png/day_cloud.png",
             height: 25,
           ),
-          const Text(
-            '16°',
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
+          Text(
+            '$temperature °C',
+            style: GoogleFonts.lato(
+              fontSize: 15,
+              fontWeight: FontWeight.w700,
             ),
           ),
-          const Text(
-            'cloudy',
-            style: TextStyle(
-              fontSize: 10,
+          const SizedBox(height: 2),
+          Text(
+            maxLines: 1,
+            weatherMain,
+            style: GoogleFonts.robotoCondensed(
+              fontSize: 11.5,
               fontWeight: FontWeight.w400,
             ),
+            // overflow: TextOverflow.fade,
           ),
           const SizedBox(height: 8),
-          const Text(
-            'Today',
-            style: TextStyle(
+          Text(
+            day,
+            maxLines: 1,
+            style: GoogleFonts.redRose(
               fontSize: 12,
               fontWeight: FontWeight.w400,
             ),
           ),
-          const Text(
-            'Luck now',
-            style: TextStyle(
-              fontSize: 10,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          const SizedBox(height: 10),
+          const SizedBox(height: 6),
         ],
       ),
     );
